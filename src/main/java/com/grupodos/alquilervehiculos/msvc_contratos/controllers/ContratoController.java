aaa3cd922606cee61649eb4f7d269c5f77f713a9
Alecraft2004 <alecraft2005yt@gmail.com>
2025-12-10 23:11:30 -0500

    Parte Final 2.1

--- src/main/java/com/grupodos/alquilervehiculos/msvc_contratos/controllers/ContratoController.java
+++ src/main/java/com/grupodos/alquilervehiculos/msvc_contratos/controllers/ContratoController.java
@@ -3,22 +3,33 @@ package com.grupodos.alquilervehiculos.msvc_contratos.controllers;
 import com.grupodos.alquilervehiculos.msvc_contratos.dto.ContratoRequestDto;
 import com.grupodos.alquilervehiculos.msvc_contratos.dto.ContratoResponseDto;
 import com.grupodos.alquilervehiculos.msvc_contratos.dto.RangoFechasRequest;
-import com.grupodos.alquilervehiculos.msvc_contratos.entities.Contrato;
 import com.grupodos.alquilervehiculos.msvc_contratos.services.ContratoService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/contratos")
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5173","https://soluciones-integrales-juri.vercel.app")
+@CrossOrigin(origins = {
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://soluciones-integrales-juri.vercel.app"
+})
 public class ContratoController {
 
-    private final ContratoService  contratoService;
+    private final ContratoService contratoService;
 
     public ContratoController(ContratoService contratoService) {
         this.contratoService = contratoService;
@@ -35,7 +46,8 @@ public class ContratoController {
     }
 
     @PostMapping
-    public ResponseEntity<ContratoResponseDto> crearContrato(@Valid @RequestBody ContratoRequestDto dto) {
+    public ResponseEntity<ContratoResponseDto> crearContrato(
+            @Valid @RequestBody ContratoRequestDto dto) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(contratoService.crearContrato(dto));
     }
@@ -66,10 +78,8 @@ public class ContratoController {
     @PostMapping("/rango-fechas")
     public ResponseEntity<List<ContratoResponseDto>> obtenerContratosPorRangoFechas(
             @Valid @RequestBody RangoFechasRequest request) {
-
         List<ContratoResponseDto> contratos = contratoService
                 .obtenerContratosPorRangoFechas(request.fechaInicio(), request.fechaFin());
-
         return ResponseEntity.ok(contratos);
     }
 }
